e2e2d3c0a6528e33fdcdcedeb9044f28ec737103
FluGhost <fluvio.371@gmail.com>
2020-03-21 18:57:58 +0100

    Add some files like the main page

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+require("indexView.php");
---
+++ indexView.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Don't Be A Fish !</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style/main.css">
+    </head>
+    <body>
+        <header>
+            <h1>Don't Be A Fish !</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Commencer Maintenant.</a></li>
+                    <li><a href="">Me contacter/Qui suis-je ?</a></li>
+                    <li><a href="https://github.com/FluGhost/Don-t-be-a-fish-Project" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png" alt="GitHub logo"> GitHub Repository</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
